46b8a49affec8c070c97ced4f4a3d56a796d26fd
Timothy Dry <timothy.dry@verizon.net>
2017-02-08 09:24:38 -0500

    changed mycharacters from an object to an array with 4 objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,35 @@
 
-myCharacters = {
-		name: ["Luke", "Obi-Wan", "Darth-Vader", "Darth-Maul"],
-		health: [100, 120, 150, 100],
-		attack: [6, 6, 6, 6],
-		counterAttack: [5, 7, 25, 20],
-		image: ["/assets/images/luke.jpeg", "/assets/images/obi-wan.jpeg", "/assets/images/darth-vader.jpeg", "/assets/images/darth-maul.jpeg"]
-	}
+var myCharacters = [{
+		name: "Luke",   
+		health: 100,
+		attack: 6, 
+		counterAttack: 5,
+		image: "/assets/images/luke.jpeg"},
+			{
+		name: "Obi-Wan",
+		health: 120,
+		attack: 6,
+		counterAttack: 7,
+		image: "/assets/images/obi-wan.jpeg"},
+			{
+		name: "Darth-Vader",
+		health: 150,
+		attack: 6,
+		counterAttack: 25,
+		image: "/assets/images/darth-vader.jpeg"},
+			{
+		name: "Darth-Maul",
+		health: 100,
+		attack: 6,
+		counterAttack: 20,
+		image: "/assets/images/darth-maul.jpeg"}
+		];
+	
 var myChamp = "";
 var myEnemies = {};
 var defender = {};
 var char = "";
+var hitPoints = 6;
 
 
 
@@ -18,22 +38,29 @@ function initialize () {
 	
 	
 }
+//var lookup = {};
+//for (var i = 0, len = myCharacters.length; i < len; i++) {
+ //   lookup[myCharacters[i].id] = array[i]; // now I can use lookup[id]
+//}
+
 $(document).ready(function() {
-	$(".characters").append(('<button id="Luke" class="btn btn-lg cbutton" value="Luke">Luke Skywalker<br><img src="assets/images/luke1.png"><br>Health:') + myCharacters.health[0] + ('</button>'));
-	
+	$(".characters").append(('<button id="Luke" class="btn btn-lg cbutton" value="Luke">Luke Skywalker<br><img src="assets/images/luke1.png"><br>Health:') + myCharacters[0].health + ('</button>'));
+	$("#Luke").val = 1;
 	//console.log(myCharacters.health[0]);
-	$(".characters").append(('<button id="Obi-Wan" class="btn btn-lg cbutton" value="Obi-Wan">Obi-Wan<br><img src="assets/images/obi-wan1.png"><br>Health:') + myCharacters.health[1] + ('</button>'));
+	$(".characters").append(('<button id="Obi-Wan" class="btn btn-lg cbutton" value="Obi-Wan">Obi-Wan<br><img src="assets/images/obi-wan1.png"><br>Health:') + myCharacters[1].health + ('</button>'));
 	//$("#Obi-Wan").val("Obi-Wan");​​​​​​
 	//console.log(myCharacters.health[1]);
-	$(".characters").append(('<button id="Darth-Vader" class="btn btn-lg cbutton" value="Darth-Vader">Darth Vader<br><img src="assets/images/darth-vader1.png"><br>Health:') + myCharacters.health[1] + ('</button>'));
-	$(".characters").append(('<button id="Darth Maul" class="btn btn-lg cbutton" value="Darth-Maul">Darth Maul<br><img src="assets/images/darth-maul1.png"><br>Health:') + myCharacters.health[1] + ('</button>'));
+	$(".characters").append(('<button id="Darth-Vader" class="btn btn-lg cbutton" value="Darth-Vader">Darth Vader<br><img src="assets/images/darth-vader1.png"><br>Health:') + myCharacters[2].health + ('</button>'));
+	$(".characters").append(('<button id="Darth Maul" class="btn btn-lg cbutton" value="Darth-Maul">Darth Maul<br><img src="assets/images/darth-maul1.png"><br>Health:') + myCharacters[3].health + ('</button>'));
 	//$("#Luke").val(Characters.name[0]);​​​​​
 	$(".characters").on("click", function () {
-		myChamp = this.value;
+		myChamp = this.characters;
 		var value = $(this).val();
 		console.log($(this).val());
 		console.log(myChamp + " " + value);
+		//console.log(lookup[name]);
 	});
+
 /*
 $(document).ready(function() {
 	initialize ();
